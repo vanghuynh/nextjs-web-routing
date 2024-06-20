51df776fdeb668c02b8c8953e5e64eb0c42e558d
Vang Huynh <huynh.vang@devexperts.com>
2024-06-20 16:38:46 +0700

    add fetch data from database

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,13 +1,40 @@
-import Link from "next/link";
+"use client";
 
-import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [error, setError] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [news, setNews] = useState();
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+      if (!response.ok) {
+        setError("Failed to fetch news");
+        setIsLoading(false);
+      }
+      const news = await response.json();
+      setIsLoading(false);
+      setNews(news);
+    }
+    fetchNews();
+  }, []);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+  if (error) {
+    return <p>{error}</p>;
+  }
+  let newsContent;
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
   return (
     <div>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </div>
   );
 }
