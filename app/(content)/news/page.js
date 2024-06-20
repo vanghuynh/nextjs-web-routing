f15e22ea5476cd46a4133a6f0f5fdeda8f45a516
Vang Huynh <huynh.vang@devexperts.com>
2024-06-20 17:34:33 +0700

    add loading for server side component with async function

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -2,7 +2,7 @@ import NewsList from "@/components/news-list";
 import { getAllNews } from "../../../lib/news";
 
 export default async function NewsPage() {
-  const news = getAllNews();
+  const news = await getAllNews();
   return (
     <div>
       <h1>News Page</h1>
