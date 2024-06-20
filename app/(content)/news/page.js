15edccf8686d0c41be2b9aa8cf26c0acf96b94d1
Vang Huynh <huynh.vang@devexperts.com>
2024-06-20 17:25:55 +0700

    load news from server components

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,40 +1,15 @@
-"use client";
-
 import NewsList from "@/components/news-list";
-import { useEffect, useState } from "react";
 
-export default function NewsPage() {
-  const [error, setError] = useState();
-  const [isLoading, setIsLoading] = useState(false);
-  const [news, setNews] = useState();
-  useEffect(() => {
-    async function fetchNews() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:8080/news");
-      if (!response.ok) {
-        setError("Failed to fetch news");
-        setIsLoading(false);
-      }
-      const news = await response.json();
-      setIsLoading(false);
-      setNews(news);
-    }
-    fetchNews();
-  }, []);
-  if (isLoading) {
-    return <p>Loading...</p>;
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+  if (!response.ok) {
+    throw new Error("Failed to fetch news");
   }
-  if (error) {
-    return <p>{error}</p>;
-  }
-  let newsContent;
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+  const news = await response.json();
   return (
     <div>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </div>
   );
 }
